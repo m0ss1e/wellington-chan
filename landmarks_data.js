7b7a442bea4e9a5a190d5545b4b2ce8109a099a8
Helena Whiteford <helena.whiteford@outlook.com>
2026-05-24 18:04:57 +1200

    Add elevation data + topographic walk-time adjustment

--- landmarks_data.js
+++ landmarks_data.js
@@ -2,431 +2,517 @@ window.LANDMARKS = {
   "auckland": {
     "lat": -36.85868366715576,
     "lng": 174.90962012907747,
-    "suburb": null
+    "suburb": null,
+    "elev": 0
   },
   "australia": {
     "lat": -25.188346861474084,
     "lng": 133.86778414880695,
-    "suburb": null
+    "suburb": null,
+    "elev": 369
   },
   "beach": {
     "lat": -41.290785881461616,
     "lng": 174.79247089405382,
-    "suburb": "Oriental Bay"
+    "suburb": "Oriental Bay",
+    "elev": 0
   },
   "beehive": {
     "lat": -41.278325854439565,
     "lng": 174.7764331728208,
-    "suburb": "Pipitea"
+    "suburb": "Pipitea",
+    "elev": 20
   },
   "botanic garden": {
     "lat": -41.28296232416743,
     "lng": 174.76656540845997,
-    "suburb": "Kelburn"
+    "suburb": "Kelburn",
+    "elev": 79
   },
   "botanic gardens": {
     "lat": -41.28296232416743,
     "lng": 174.76656540845997,
-    "suburb": "Kelburn"
+    "suburb": "Kelburn",
+    "elev": 79
   },
   "brisbane": {
     "lat": -27.484380737235668,
     "lng": 153.7370629719385,
-    "suburb": null
+    "suburb": null,
+    "elev": 0
   },
   "bucket fountain": {
     "lat": -41.29208638701385,
     "lng": 174.7762932963523,
-    "suburb": "Te Aro"
+    "suburb": "Te Aro",
+    "elev": 14
   },
   "carterton": {
     "lat": -41.02612588175024,
     "lng": 175.52538117896162,
-    "suburb": null
+    "suburb": null,
+    "elev": 77
   },
   "cbd": {
     "lat": -41.28197994419336,
     "lng": 174.7760167041259,
-    "suburb": "Wellington Central"
+    "suburb": "Wellington Central",
+    "elev": 12
   },
   "christchurch": {
     "lat": -43.544321402501375,
     "lng": 172.8350012745755,
-    "suburb": null
+    "suburb": null,
+    "elev": 0
   },
   "courtenay": {
     "lat": -41.293292778156335,
     "lng": 174.78121119635242,
-    "suburb": "Te Aro"
+    "suburb": "Te Aro",
+    "elev": 13
   },
   "courtenay place": {
     "lat": -41.293292778156335,
     "lng": 174.78121119635242,
-    "suburb": "Te Aro"
+    "suburb": "Te Aro",
+    "elev": 13
   },
   "cuba": {
     "lat": -41.293503916000546,
     "lng": 174.7755923967682,
-    "suburb": "Te Aro"
+    "suburb": "Te Aro",
+    "elev": 16
   },
   "cuba mall": {
     "lat": -41.293503916000546,
     "lng": 174.7755923967682,
-    "suburb": "Te Aro"
+    "suburb": "Te Aro",
+    "elev": 16
   },
   "cuba street": {
     "lat": -41.293503916000546,
     "lng": 174.7755923967682,
-    "suburb": "Te Aro"
+    "suburb": "Te Aro",
+    "elev": 16
   },
   "design campus": {
     "lat": -41.295535512071886,
     "lng": 174.7756896651286,
-    "suburb": "Te Aro"
+    "suburb": "Te Aro",
+    "elev": 20
   },
   "dunedin": {
     "lat": -45.88754900600229,
     "lng": 170.6522328349189,
-    "suburb": null
+    "suburb": null,
+    "elev": 49
   },
   "embassy": {
     "lat": -41.29421756769906,
     "lng": 174.78415805387323,
-    "suburb": "Mount Victoria"
+    "suburb": "Mount Victoria",
+    "elev": 12
   },
   "embassy theatre": {
     "lat": -41.29421756769906,
     "lng": 174.78415805387323,
-    "suburb": "Mount Victoria"
+    "suburb": "Mount Victoria",
+    "elev": 12
   },
   "featherston": {
     "lat": -41.11671565705571,
     "lng": 175.32556734064045,
-    "suburb": null
+    "suburb": null,
+    "elev": 45
   },
   "greytown": {
     "lat": -41.07945954858668,
     "lng": 175.46358308463545,
-    "suburb": null
+    "suburb": null,
+    "elev": 56
   },
   "hamilton": {
     "lat": -37.79824918109403,
     "lng": 175.38844171139462,
-    "suburb": null
+    "suburb": null,
+    "elev": 50
   },
   "hnry stadium": {
     "lat": -41.272836359150645,
     "lng": 174.78507391184507,
-    "suburb": "Pipitea"
+    "suburb": "Pipitea",
+    "elev": 12
   },
   "hospital": {
     "lat": -41.30873006205505,
     "lng": 174.77903659635317,
-    "suburb": "Newtown"
+    "suburb": "Newtown",
+    "elev": 19
   },
   "kelburn campus": {
     "lat": -41.289192168200486,
     "lng": 174.76822612079417,
-    "suburb": "Kelburn"
+    "suburb": "Kelburn",
+    "elev": 106
   },
   "la": {
     "lat": 34.10869015861665,
     "lng": -119.33244435635339,
-    "suburb": null
+    "suburb": null,
+    "elev": 0
   },
   "lambton quay": {
     "lat": -41.28197994419336,
     "lng": 174.7760167041259,
-    "suburb": "Wellington Central"
+    "suburb": "Wellington Central",
+    "elev": 12
   },
   "london": {
     "lat": 51.531869777188554,
     "lng": -0.11573149517306795,
-    "suburb": null
+    "suburb": null,
+    "elev": 30
   },
   "los angeles": {
     "lat": 34.10869015861665,
     "lng": -119.33244435635339,
-    "suburb": null
+    "suburb": null,
+    "elev": 0
   },
   "lower hutt": {
     "lat": -41.21322432651201,
     "lng": 174.9005789685164,
-    "suburb": null
+    "suburb": null,
+    "elev": 6
   },
   "massey": {
     "lat": -41.3018890051744,
     "lng": 174.77539459934826,
-    "suburb": "Mount Cook"
+    "suburb": "Mount Cook",
+    "elev": 41
   },
   "massey campus": {
     "lat": -41.3018890051744,
     "lng": 174.77539459934826,
-    "suburb": "Mount Cook"
+    "suburb": "Mount Cook",
+    "elev": 41
   },
   "massey uni": {
     "lat": -41.3018890051744,
     "lng": 174.77539459934826,
-    "suburb": "Mount Cook"
+    "suburb": "Mount Cook",
+    "elev": 41
   },
   "massey university": {
     "lat": -41.3018890051744,
     "lng": 174.77539459934826,
-    "suburb": "Mount Cook"
+    "suburb": "Mount Cook",
+    "elev": 41
   },
   "masterton": {
     "lat": -40.94526635754627,
     "lng": 175.6654568643148,
-    "suburb": null
+    "suburb": null,
+    "elev": 111
   },
   "melbourne": {
     "lat": -37.805013190267665,
     "lng": 145.8482963780392,
-    "suburb": null
+    "suburb": null,
+    "elev": 818
   },
   "mount ruapehu": {
     "lat": -39.27774743238107,
     "lng": 175.55741016657655,
-    "suburb": null
+    "suburb": null,
+    "elev": 2608
   },
   "nelson": {
     "lat": -41.29853070804219,
     "lng": 173.30393533074914,
-    "suburb": null
+    "suburb": null,
+    "elev": 291
   },
   "new york": {
     "lat": 40.714000504398214,
     "lng": -73.78064866264894,
-    "suburb": null
+    "suburb": null,
+    "elev": 28
   },
   "oriental beach": {
     "lat": -41.290785881461616,
     "lng": 174.79247089405382,
-    "suburb": "Oriental Bay"
+    "suburb": "Oriental Bay",
+    "elev": 0
   },
   "palmerston north": {
     "lat": -40.33522841198177,
     "lng": 175.69139818152317,
-    "suburb": null
+    "suburb": null,
+    "elev": 42
   },
   "palmy": {
     "lat": -40.33522841198177,
     "lng": 175.69139818152317,
-    "suburb": null
+    "suburb": null,
+    "elev": 42
   },
   "paris": {
     "lat": 48.881610832894616,
     "lng": 3.0384630986878274,
-    "suburb": null
+    "suburb": null,
+    "elev": 147
   },
   "parliament": {
     "lat": -41.278325854439565,
     "lng": 174.7764331728208,
-    "suburb": "Pipitea"
+    "suburb": "Pipitea",
+    "elev": 20
   },
   "petone": {
     "lat": -41.229906403636974,
     "lng": 174.87907124321956,
-    "suburb": null
+    "suburb": null,
+    "elev": 0
   },
   "pipitea campus": {
     "lat": -41.278673180240766,
     "lng": 174.7790860256339,
-    "suburb": "Pipitea"
+    "suburb": "Pipitea",
+    "elev": 10
   },
   "porirua": {
     "lat": -41.137949804193816,
     "lng": 174.84454077500573,
-    "suburb": null
+    "suburb": null,
+    "elev": 6
   },
   "ruapehu": {
     "lat": -39.27774743238107,
     "lng": 175.55741016657655,
-    "suburb": null
+    "suburb": null,
+    "elev": 2608
   },
   "stuart island": {
     "lat": -46.98809380205638,
     "lng": 167.93365247126192,
-    "suburb": null
+    "suburb": null,
+    "elev": 431
   },
   "sydney": {
     "lat": -33.89773953119155,
     "lng": 151.8764188716381,
-    "suburb": null
+    "suburb": null,
+    "elev": 0
   },
   "taupo": {
     "lat": -38.69168808368944,
     "lng": 176.13295433608087,
-    "suburb": null
+    "suburb": null,
+    "elev": 525
   },
   "te aro campus": {
     "lat": -41.295535512071886,
     "lng": 174.7756896651286,
-    "suburb": "Te Aro"
+    "suburb": "Te Aro",
+    "elev": 20
   },
   "te papa": {
     "lat": -41.290344280873086,
     "lng": 174.78239685347864,
-    "suburb": "Te Aro"
+    "suburb": "Te Aro",
+    "elev": 13
   },
   "te papa museum": {
     "lat": -41.290344280873086,
     "lng": 174.78239685347864,
-    "suburb": "Te Aro"
+    "suburb": "Te Aro",
+    "elev": 13
   },
   "the airport": {
     "lat": -41.328253114895965,
     "lng": 174.80868924312048,
-    "suburb": "Rongotai"
+    "suburb": "Rongotai",
+    "elev": 4
   },
   "the beach": {
     "lat": -41.290785881461616,
     "lng": 174.79247089405382,
-    "suburb": "Oriental Bay"
+    "suburb": "Oriental Bay",
+    "elev": 0
   },
   "the beehive": {
     "lat": -41.278325854439565,
     "lng": 174.7764331728208,
-    "suburb": "Pipitea"
+    "suburb": "Pipitea",
+    "elev": 20
   },
   "the botanic garden": {
     "lat": -41.28296232416743,
     "lng": 174.76656540845997,
-    "suburb": "Kelburn"
+    "suburb": "Kelburn",
+    "elev": 79
   },
   "the botanic gardens": {
     "lat": -41.28296232416743,
     "lng": 174.76656540845997,
-    "suburb": "Kelburn"
+    "suburb": "Kelburn",
+    "elev": 79
   },
   "the city centre": {
     "lat": -41.28197994419336,
     "lng": 174.7760167041259,
-    "suburb": "Wellington Central"
+    "suburb": "Wellington Central",
+    "elev": 12
   },
   "the design campus": {
     "lat": -41.295535512071886,
     "lng": 174.7756896651286,
-    "suburb": "Te Aro"
+    "suburb": "Te Aro",
+    "elev": 20
   },
   "the embassy": {
     "lat": -41.29421756769906,
     "lng": 174.78415805387323,
-    "suburb": "Mount Victoria"
+    "suburb": "Mount Victoria",
+    "elev": 12
   },
   "the gardens": {
     "lat": -41.28296232416743,
     "lng": 174.76656540845997,
-    "suburb": "Kelburn"
+    "suburb": "Kelburn",
+    "elev": 79
   },
   "the hospital": {
     "lat": -41.30873006205505,
     "lng": 174.77903659635317,
-    "suburb": "Newtown"
+    "suburb": "Newtown",
+    "elev": 19
   },
   "the kelburn campus": {
     "lat": -41.289192168200486,
     "lng": 174.76822612079417,
-    "suburb": "Kelburn"
+    "suburb": "Kelburn",
+    "elev": 106
   },
   "the museum": {
     "lat": -41.290344280873086,
     "lng": 174.78239685347864,
-    "suburb": "Te Aro"
+    "suburb": "Te Aro",
+    "elev": 13
   },
   "the stadium": {
     "lat": -41.272836359150645,
     "lng": 174.78507391184507,
-    "suburb": "Pipitea"
+    "suburb": "Pipitea",
+    "elev": 12
   },
   "the station": {
     "lat": -41.27920626968712,
     "lng": 174.78012851333085,
-    "suburb": "Pipitea"
+    "suburb": "Pipitea",
+    "elev": 11
   },
   "the te aro campus": {
     "lat": -41.295535512071886,
     "lng": 174.7756896651286,
-    "suburb": "Te Aro"
+    "suburb": "Te Aro",
+    "elev": 20
   },
   "the zoo": {
     "lat": -41.319214457961635,
     "lng": 174.78337633196634,
-    "suburb": "Newtown"
+    "suburb": "Newtown",
+    "elev": 43
   },
   "tokyo": {
     "lat": 35.72278491691652,
     "lng": 139.98780898028804,
-    "suburb": null
+    "suburb": null,
+    "elev": 12
   },
   "train station": {
     "lat": -41.27920626968712,
     "lng": 174.78012851333085,
-    "suburb": "Pipitea"
+    "suburb": "Pipitea",
+    "elev": 11
   },
   "upper hutt": {
     "lat": -41.12602638316662,
     "lng": 175.06309223706432,
-    "suburb": null
+    "suburb": null,
+    "elev": 60
   },
   "vic uni": {
     "lat": -41.289192168200486,
     "lng": 174.76822612079417,
-    "suburb": "Kelburn"
+    "suburb": "Kelburn",
+    "elev": 106
   },
   "victoria university": {
     "lat": -41.289192168200486,
     "lng": 174.76822612079417,
-    "suburb": "Kelburn"
+    "suburb": "Kelburn",
+    "elev": 106
   },
   "vuw": {
     "lat": -41.289192168200486,
     "lng": 174.76822612079417,
-    "suburb": "Kelburn"
+    "suburb": "Kelburn",
+    "elev": 106
   },
   "waitangi park": {
     "lat": -41.29149869299518,
     "lng": 174.78514319044564,
-    "suburb": "Te Aro"
+    "suburb": "Te Aro",
+    "elev": 4
   },
   "waterfront": {
     "lat": -41.288779172735325,
     "lng": 174.78014427868104,
-    "suburb": "Wellington Central"
+    "suburb": "Wellington Central",
+    "elev": 0
   },
   "wellington airport": {
     "lat": -41.328253114895965,
     "lng": 174.80868924312048,
-    "suburb": "Rongotai"
+    "suburb": "Rongotai",
+    "elev": 4
   },
   "wellington botanic garden": {
     "lat": -41.28296232416743,
     "lng": 174.76656540845997,
-    "suburb": "Kelburn"
+    "suburb": "Kelburn",
+    "elev": 79
   },
   "wellington hospital": {
     "lat": -41.30873006205505,
     "lng": 174.77903659635317,
-    "suburb": "Newtown"
+    "suburb": "Newtown",
+    "elev": 19
   },
   "wellington station": {
     "lat": -41.27920626968712,
     "lng": 174.78012851333085,
-    "suburb": "Pipitea"
+    "suburb": "Pipitea",
+    "elev": 11
   },
   "wellington zoo": {
     "lat": -41.319214457961635,
     "lng": 174.78337633196634,
-    "suburb": "Newtown"
+    "suburb": "Newtown",
+    "elev": 43
   },
   "zealandia": {
     "lat": -41.29094190333012,
     "lng": 174.7534634189363,
-    "suburb": "Karori"
+    "suburb": "Karori",
+    "elev": 148
   },
   "zoo": {
     "lat": -41.319214457961635,
     "lng": 174.78337633196634,
-    "suburb": "Newtown"
+    "suburb": "Newtown",
+    "elev": 43
   }
 };
--- wellington_data.js
+++ wellington_data.js
@@ -2,266 +2,319 @@ window.SUBURBS = {
   "Aro Valley": {
     "lat": -41.294762,
     "lng": 174.764041,
-    "stops": 12
+    "stops": 12,
+    "elev": 57
   },
   "Berhampore": {
     "lat": -41.321641,
     "lng": 174.777392,
-    "stops": 20
+    "stops": 20,
+    "elev": 43
   },
   "Breaker Bay": {
     "lat": -41.336002,
     "lng": 174.825306,
-    "stops": 13
+    "stops": 13,
+    "elev": 0
   },
   "Broadmeadows": {
     "lat": -41.235482,
     "lng": 174.795066,
-    "stops": 18
+    "stops": 18,
+    "elev": 240
   },
   "Brooklyn": {
     "lat": -41.308194,
     "lng": 174.762039,
-    "stops": 58
+    "stops": 58,
+    "elev": 138
   },
   "Churton Park": {
     "lat": -41.205621,
     "lng": 174.806629,
-    "stops": 40
+    "stops": 40,
+    "elev": 106
   },
   "Crofton Downs": {
     "lat": -41.257277,
     "lng": 174.765855,
-    "stops": 4
+    "stops": 4,
+    "elev": 86
   },
   "Glenside": {
     "lat": -41.198683,
     "lng": 174.820916,
-    "stops": 9
+    "stops": 9,
+    "elev": 72
   },
   "Grenada North": {
     "lat": -41.184688,
     "lng": 174.836285,
-    "stops": 9
+    "stops": 9,
+    "elev": 88
   },
   "Grenada Village": {
     "lat": -41.206216,
     "lng": 174.823314,
-    "stops": 10
+    "stops": 10,
+    "elev": 164
   },
   "Hataitai": {
     "lat": -41.303663,
     "lng": 174.797822,
-    "stops": 47
+    "stops": 47,
+    "elev": 50
   },
   "Highbury": {
     "lat": -41.292191,
     "lng": 174.756596,
-    "stops": 4
+    "stops": 4,
+    "elev": 181
   },
   "Houghton Bay": {
     "lat": -41.337198,
     "lng": 174.786691,
-    "stops": 20
+    "stops": 20,
+    "elev": 62
   },
   "Island Bay": {
     "lat": -41.339593,
     "lng": 174.770233,
-    "stops": 49
+    "stops": 49,
+    "elev": 6
   },
   "Johnsonville": {
     "lat": -41.222872,
     "lng": 174.801579,
-    "stops": 61
+    "stops": 61,
+    "elev": 168
   },
   "Kaiwharawhara": {
     "lat": -41.258165,
     "lng": 174.789135,
-    "stops": 9
+    "stops": 9,
+    "elev": 53
   },
   "Karaka Bays": {
     "lat": -41.304014,
     "lng": 174.830588,
-    "stops": 20
+    "stops": 20,
+    "elev": 55
   },
   "Karori": {
     "lat": -41.286644,
     "lng": 174.736495,
-    "stops": 97
+    "stops": 97,
+    "elev": 165
   },
   "Kelburn": {
     "lat": -41.287291,
     "lng": 174.760704,
-    "stops": 32
+    "stops": 32,
+    "elev": 147
   },
   "Khandallah": {
     "lat": -41.248237,
     "lng": 174.794896,
-    "stops": 62
+    "stops": 62,
+    "elev": 166
   },
   "Kilbirnie": {
     "lat": -41.316883,
     "lng": 174.795159,
-    "stops": 22
+    "stops": 22,
+    "elev": 4
   },
   "Kingston": {
     "lat": -41.322705,
     "lng": 174.764712,
-    "stops": 8
+    "stops": 8,
+    "elev": 165
   },
   "Lyall Bay": {
     "lat": -41.330051,
     "lng": 174.793047,
-    "stops": 16
+    "stops": 16,
+    "elev": 4
   },
   "Maupuia": {
     "lat": -41.304235,
     "lng": 174.819842,
-    "stops": 22
+    "stops": 22,
+    "elev": 89
   },
   "Melrose": {
     "lat": -41.327066,
     "lng": 174.787179,
-    "stops": 10
+    "stops": 10,
+    "elev": 117
   },
   "Miramar": {
     "lat": -41.315087,
     "lng": 174.820255,
-    "stops": 76
+    "stops": 76,
+    "elev": 3
   },
   "Moa Point": {
     "lat": -41.337451,
     "lng": 174.810741,
-    "stops": 2
+    "stops": 2,
+    "elev": 8
   },
   "Mornington": {
     "lat": -41.317928,
     "lng": 174.76735,
-    "stops": 13
+    "stops": 13,
+    "elev": 146
   },
   "Mount Cook": {
     "lat": -41.302664,
     "lng": 174.774801,
-    "stops": 18
+    "stops": 18,
+    "elev": 39
   },
   "Mount Victoria": {
     "lat": -41.29672,
     "lng": 174.787187,
-    "stops": 22
+    "stops": 22,
+    "elev": 20
   },
   "Newlands": {
     "lat": -41.225516,
     "lng": 174.823852,
-    "stops": 45
+    "stops": 45,
+    "elev": 194
   },
   "Newtown": {
     "lat": -41.313969,
     "lng": 174.780763,
-    "stops": 33
+    "stops": 33,
+    "elev": 26
   },
   "Ngaio": {
     "lat": -41.252098,
     "lng": 174.776101,
-    "stops": 37
+    "stops": 37,
+    "elev": 135
   },
   "Ngauranga": {
     "lat": -41.245404,
     "lng": 174.812082,
-    "stops": 7
+    "stops": 7,
+    "elev": 41
   },
   "Northland": {
     "lat": -41.281425,
     "lng": 174.758251,
-    "stops": 19
+    "stops": 19,
+    "elev": 148
   },
   "Oriental Bay": {
     "lat": -41.290473,
     "lng": 174.794375,
-    "stops": 12
+    "stops": 12,
+    "elev": 0
   },
   "Owhiro Bay": {
     "lat": -41.337926,
     "lng": 174.75947,
-    "stops": 22
+    "stops": 22,
+    "elev": 27
   },
   "Paparangi": {
     "lat": -41.216843,
     "lng": 174.818094,
-    "stops": 30
+    "stops": 30,
+    "elev": 165
   },
   "Pipitea": {
     "lat": -41.276446,
     "lng": 174.780338,
-    "stops": 29
+    "stops": 29,
+    "elev": 20
   },
   "Rongotai": {
     "lat": -41.324927,
     "lng": 174.803765,
-    "stops": 7
+    "stops": 7,
+    "elev": 7
   },
   "Roseneath": {
     "lat": -41.292366,
     "lng": 174.800703,
-    "stops": 30
+    "stops": 30,
+    "elev": 86
   },
   "Seatoun": {
     "lat": -41.32135,
     "lng": 174.83032,
-    "stops": 20
+    "stops": 20,
+    "elev": 6
   },
   "Southgate": {
     "lat": -41.337007,
     "lng": 174.781968,
-    "stops": 16
+    "stops": 16,
+    "elev": 93
   },
   "Strathmore Park": {
     "lat": -41.330626,
     "lng": 174.820324,
-    "stops": 41
+    "stops": 41,
+    "elev": 33
   },
   "Takapu Valley": {
     "lat": -41.176117,
     "lng": 174.841712,
-    "stops": 2
+    "stops": 2,
+    "elev": 105
   },
   "Tawa": {
     "lat": -41.170267,
     "lng": 174.826602,
-    "stops": 96
+    "stops": 96,
+    "elev": 29
   },
   "Te Aro": {
     "lat": -41.293137,
     "lng": 174.776666,
-    "stops": 30
+    "stops": 30,
+    "elev": 16
   },
   "Thorndon": {
     "lat": -41.273493,
     "lng": 174.776548,
-    "stops": 14
+    "stops": 14,
+    "elev": 21
   },
   "Vogeltown": {
     "lat": -41.312931,
     "lng": 174.770444,
-    "stops": 6
+    "stops": 6,
+    "elev": 100
   },
   "Wadestown": {
     "lat": -41.263616,
     "lng": 174.772467,
-    "stops": 25
+    "stops": 25,
+    "elev": 149
   },
   "Wellington Central": {
     "lat": -41.283114,
     "lng": 174.775693,
-    "stops": 23
+    "stops": 23,
+    "elev": 17
   },
   "Wilton": {
     "lat": -41.269537,
     "lng": 174.760015,
-    "stops": 30
+    "stops": 30,
+    "elev": 119
   },
   "Woodridge": {
     "lat": -41.21721,
     "lng": 174.831725,
-    "stops": 9
+    "stops": 9,
+    "elev": 214
   }
 };
